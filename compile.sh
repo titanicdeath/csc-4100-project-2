1e9eb159e39b412a55ab0f6a1810f20392b3203a
titanicdeath <aldensimmons3@gmail.com>
2026-04-26 20:12:19 -0500

    adjusted compiled script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+set -e
 
-cc -o timer timer.c -Wall -Werror
+PINTOS=/home/os/path/to/pintos    # ← fills in once we know
 
+# Copy authored files into the source tree
+cp timer.c     "$PINTOS/src/devices/timer.c"
+cp thread.h    "$PINTOS/src/threads/thread.h"
+cp thread.c    "$PINTOS/src/threads/thread.c"
+cp DESIGNDOC   "$PINTOS/src/threads/DESIGNDOC"
+
+# Build
+cd "$PINTOS/src/threads"
+make
+
+echo "Build complete. To run tests: cd $PINTOS/src/threads/build && make grade"
